fa654b7e8b8dc0d46d96c3ff19c721c58b89925f
Daviti Vashakmadze <vashaymadzedavit@gmail.comgit config --global user.name Datuchaaa>
2023-06-13 19:36:36 +0400

    fix: improve code

---
+++ src/components/Signupform.jsx
@@ -0,0 +1,111 @@
+import React from "react";
+import { Formik, Field, ErrorMessage, Form } from "formik";
+import * as Yup from "yup";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faEye,
+  faEyeSlash,
+  faCircleExclamation,
+} from "@fortawesome/free-solid-svg-icons";
+
+const SignupForm = () => {
+  return (
+    <Formik
+      initialValues={{
+        email: "",
+        password: "",
+        name: "",
+        lastName: "",
+      }}
+      onSubmit={async (values) => {
+        await new Promise((resolve) => setTimeout(resolve, 500));
+        alert(JSON.stringify(values, null, 2));
+      }}
+      validationSchema={Yup.object().shape({
+        name: Yup.string().required("First Name cannot be empty"),
+        lastName: Yup.string().required("Last Name cannot be empty"),
+        email: Yup.string()
+          .email("Looks like this is not an email")
+          .required("Required"),
+        password: Yup.string()
+          .min(8, "Password must be at least 8 characters")
+          .matches(
+            /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/,
+            "Password Requred Special character and cemal case"
+          )
+          .required("Password cannot be empty"),
+      })}
+    >
+      {({ values, touched, errors, handleChange, handleBlur }) => (
+        <Form>
+          <Field
+            id="name"
+            name="name"
+            placeholder="First Name"
+            type="text"
+            className={
+              errors.name && touched.name ? "text-input error" : "text-input"
+            }
+          />
+
+          <ErrorMessage
+            name="name"
+            component="div"
+            className="input-feedback"
+          />
+          <Field
+            id="lastName"
+            name="lastName"
+            placeholder="Last Name"
+            type="text"
+            className={
+              errors.lastName && touched.lastName
+                ? "text-input error"
+                : "text-input"
+            }
+          />
+
+          <ErrorMessage
+            name="lastName"
+            component="div"
+            className="input-feedback"
+          />
+          <Field
+            id="email"
+            name="email"
+            placeholder="Enter your email"
+            type="text"
+            className={
+              errors.email && touched.email ? "text-input error" : "text-input"
+            }
+          />
+
+          <ErrorMessage
+            name="email"
+            component="div"
+            className="input-feedback"
+          />
+          <Field
+            id="password"
+            name="password"
+            placeholder="Password"
+            type="password"
+            className={
+              errors.password && touched.password
+                ? "text-input error"
+                : "text-input"
+            }
+          />
+          <ErrorMessage
+            name="password"
+            component="div"
+            className="input-feedback"
+          />
+          <button type="submit">CLAIM YOUR FREE TRIAL</button>
+        </Form>
+      )}
+    </Formik>
+  );
+};
+
+export default SignupForm;
